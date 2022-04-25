576ca922ea05de67548b5d1ed90dff95fb78c8d6
antonioqc <antonio.quecua00@gmail.com>
2022-04-25 20:28:50 +0200

    home blade

--- gestor_tareas/resources/views/home.blade.php
+++ gestor_tareas/resources/views/home.blade.php
@@ -12,14 +12,14 @@
     <div class="row">
         <div class="text-center" id="allTasks">           
 
-            @php
+           {{--@php
                 $total = $tareas->count();
                 echo '<input type="hidden" value="'.$total.'" style="display: none;" id="totaltarea">';
-            @endphp
+            @endphp--}}
 
         @foreach ($tareas as $tarea)
-            <input type="hidden" name="idtarea" value="{{$tarea->id}}" style="display: none;" id="idtarea">
-            <input type="hidden" name="nombretarea" value="{{$tarea->name}}" style="display: none;" id="nombretarea">
+            {{--<input type="hidden" name="idtarea" value="{{$tarea->id}}" style="display: none;" id="idtarea">
+            <input type="hidden" name="nombretarea" value="{{$tarea->name}}" style="display: none;" id="nombretarea">--}}
                     <div class="tarea mb-2 fs-4">{{$tarea->name}}</div>
                     <div class="descripcion mb-2" id="descripciontarea">{{$tarea->description}}</div>
                     <div class="iconos">
@@ -81,31 +81,32 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script>
     $(document).ready(function () {
-            idtarea = $('#idtarea').val();
+            /* idtarea = $('#idtarea').val();
             nombreTarea = $('#nombretarea').val();
             totalTarea = $('#totaltarea').val();
             
             for(i=0;i<totalTarea;i++){
                 $(".tarea").attr("id","tarea"+idtarea);
-            }
+            }*/
             
             $('#buttonAddTask').on('click', function () {
                 $('#divTask').css('display','block');
+                $('#allTasks').css('display','none');
+            });
 
+            $('.boton-cerrar').on('click',function() {
+                $('#divTask').css('display','none');
                 $('#allTasks').css('display','block');
             });
             
             $('.descripcion, .iconos').css('display','none');
             
-            $('#tarea'+idtarea).on('click', function () {
+            /*$('#tarea'+idtarea).on('click', function () {
                 $('#descripciontarea').css('display','block');
                 $('.iconos').css('display','grid')
                             .css('grid-template-columns', 'repeat(2,1fr)')
-            });
+            });*/
  
             $('#nombre, #descripcion, #finalizacion').val("");
     });
 </script>
-
-
-
