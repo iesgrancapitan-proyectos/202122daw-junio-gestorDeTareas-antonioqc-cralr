3c50786ba322757003477895466ae6d2eb3f68f6
Rafael Miguel Cruz Álvarez <rafamcruza10@gmail.com>
2022-04-27 20:10:57 +0200

    Mostrar Tareas con sus respectivos datos

--- gestor_tareas/resources/views/home.blade.php
+++ gestor_tareas/resources/views/home.blade.php
@@ -9,29 +9,17 @@
             <button class="btn btn-primary" type="button" id="buttonAddProyect">Añadir Proyecto</button>
         </div>
     </div>
-    <br></br>
-    <div class="row">
-        <div class="text-center" id="allTasks">           
-
-           @php
-                $total = $tareas->count();
-                echo '<input type="hidden" value="'.$total.'" style="display: none;" id="totaltarea">';
-            @endphp
-
-        @foreach ($tareas as $tarea)
-            <input type="hidden" name="idtarea" value="{{$tarea->id}}" style="display: none;" id="idtarea">
-            <input type="hidden" name="nombretarea" value="{{$tarea->name}}" style="display: none;" id="nombretarea">
-                    <div class="tarea mb-2 fs-4">{{$tarea->name}}</div>
-                    <div class="descripcion mb-2" id="descripciontarea">{{$tarea->description}}</div>
-                    <div class="iconos">
-                        <div>{{$tarea->date_finally}}</div>
-                        <div>
-                            <a href=""><img src="assets/img/lapiz.svg"></a>
-                            <a href=""><img src="assets/img/eliminar.svg"></a>
-                            <a href=""><img src="assets/img/comentario.png"></a>
+    <br>
+    <div class="row">           
+        <div class="">
+            @foreach ($tareas as $tarea)
+                    <div class="tarea mb-2 fs-4" data-bs-toggle="collapse" href="#collapse-{{$tarea->id}}" aria-expanded="false"  aria-controls="collapse-{{$tarea->id}}">{{$tarea->name}}</div>
+                    <div class="collapse" id="collapse-{{$tarea->id}}">
+                        <div class="card card-body col-sm-6">
+                            Descripción: {{$tarea->description}}<br>
+                            Fecha Finalización: {{\Carbon\Carbon::parse($tarea->date_finally)->format('d/m/Y')}}
                         </div>
                     </div>
-                    <hr>
             @endforeach
         </div>
     </div>
@@ -119,14 +107,6 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script>
     $(document).ready(function () {
-            idtarea = $('#idtarea').val();
-            nombreTarea = $('#nombretarea').val();
-            totalTarea = $('#totaltarea').val();
-            
-            for(i=0;i<totalTarea;i++){
-                $(".tarea").attr("id","tarea"+idtarea);
-            }
-            
             $('#buttonAddTask').on('click', function () {
                 $('#divTask').css('display','block');
                 $('#divProyect').css('display','none');
@@ -146,12 +126,6 @@
             });
             
             $('.descripcion, .iconos').css('display','none');
-            
-            $('#tarea'+idtarea).on('click', function () {
-                $('#descripciontarea').css('display','block');
-                $('.iconos').css('display','grid')
-                            .css('grid-template-columns', 'repeat(2,1fr)')
-            });
  
             $('#nombre, #descripcion, #finalizacion').val("");
 
