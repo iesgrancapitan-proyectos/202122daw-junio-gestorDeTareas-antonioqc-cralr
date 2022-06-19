02c61d6807ff676e994f926bcff0e51883e70814
antonioqc <antonio.quecua00@gmail.com>
2022-06-19 19:07:20 +0200

    pagina inicio css

--- gestor_tareas/resources/views/welcome.blade.php
+++ gestor_tareas/resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
         <meta name="author" content="" />
         <title>WorkFine</title>
         <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="assets/img/logo.png"/>
         <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
         <!-- Google fonts-->
